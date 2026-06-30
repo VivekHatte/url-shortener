ecd0d077ecc7c30a2a0d5744cb7faecdf53ffec4
Vivek Hatte <vivek.hatte@gmail.com>
2026-06-30 12:33:57 -0700

    Adding db init script

---
+++ backend/migrations/001_init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS urls (
+
+	id		BIGSERIAL PRIMARY KEY,
+	code		VARCHAR(32) UNIQUE,
+	long_url	TEXT NOT NULL,
+	clicks		BIGINT NOT NULL DEFAULT 0,
+	created_at	TIMESTAMPZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX IF NOT EXISTS idx_urls_code ON urls(code);
